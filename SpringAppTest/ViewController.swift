97813aceec1efaaa6e68fadde3af713e5bf251e4
Vladislav Rudenko <vladisrud@gmail.com>
2024-04-17 15:50:19 +0400

    Initial Commit

---
+++ SpringAppTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringAppTest
+//
+//  Created by Влад Руденко on 17.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
